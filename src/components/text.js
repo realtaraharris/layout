1b9ecdb4ba273b5869d79002080024d265a2d958
Max Harris <harris.max@gmail.com>
2019-09-11 13:18:44 -0700

    first step to single-pass text layout

--- src/components/text.js
+++ src/components/text.js
@@ -107,14 +107,105 @@ function getRowBoxes(i, stepHeight, width, polygons) {
   return boxes
 }
 
-function textPolyCutup(polygons, stepHeight, width, height) {
-  let result = []
-  const steps = Math.floor(height / stepHeight)
-  for (let i = 0; i < steps - 1; i++) {
-    result.push(...getRowBoxes(i, stepHeight, width, polygons))
+function processBox(
+  renderContext,
+  box,
+  {startX, endX, startY, endY, lineIndex},
+  tokens,
+  tracking,
+  lineHeight,
+  spaceWidth,
+  dashWidth,
+  showBoxes
+) {
+  const finalX = box.x + startX
+  const finalY = box.y + startY
+  const finalW = endX - startX
+  const finalH = endY - startY
+
+  let tempWidth = 0
+
+  if (showBoxes) {
+    renderContext.strokeStyle = 'teal'
+    renderContext.strokeRect(finalX, finalY, finalW, finalH)
   }
 
-  return result
+  while (tempWidth < finalW) {
+    const token2 = tokens[tracking.tokenCursor]
+    if (!token2) {
+      break
+    }
+
+    if (token2.token === '\n') {
+      tracking.tokenCursor++ // don't render the newline token
+      tracking.lastLineVisited = lineIndex + 1
+      break // skip to the next box
+    }
+
+    if (lineIndex < tracking.lastLineVisited) {
+      break
+    }
+
+    if (token2.type === 'word') {
+      if (tempWidth > finalW - token2.width) {
+        tracking.lastLineVisited = lineIndex
+        break
+      }
+
+      renderContext.fillText(
+        token2.token,
+        finalX + tempWidth,
+        finalY + lineHeight
+      )
+
+      // if (showBoxes) {
+      //   renderContext.strokeStyle = 'rgba(127, 63, 195, 0.6)'
+      //   renderContext.strokeRect(finalX + tempWidth, finalY, token2.width, 20)
+      // }
+
+      tempWidth += token2.width + spaceWidth
+      tracking.tokenCursor++
+    } else if (token2.type === 'syllables') {
+      while (token2.token[tracking.syllableCounter]) {
+        const syl = token2.token[tracking.syllableCounter]
+        const meas = token2.measurements[tracking.syllableCounter].width
+
+        if (tempWidth + meas + dashWidth <= finalW) {
+          if (showBoxes) {
+            renderContext.strokeStyle = 'rgba(127, 63, 195, 0.6)'
+            renderContext.strokeRect(
+              finalX + tempWidth,
+              finalY,
+              meas,
+              lineHeight
+            )
+          }
+
+          renderContext.fillText(syl, finalX + tempWidth, finalY + lineHeight)
+          tempWidth += meas
+          tracking.syllableCounter++
+        } else if (tracking.syllableCounter > 0) {
+          renderContext.fillText('-', finalX + tempWidth, finalY + lineHeight)
+          tracking.lastLineVisited = lineIndex
+          break
+        } else {
+          tracking.lastLineVisited = lineIndex
+          break
+        }
+      }
+
+      if (tracking.syllableCounter >= token2.token.length) {
+        tracking.syllableCounter = 0
+        tracking.tokenCursor++
+        tempWidth += spaceWidth
+      } else {
+        tracking.lastLineVisited = lineIndex
+        break
+      }
+    } else {
+      console.error('invalid type:', token2.type)
+    }
+  }
 }
 
 class Text extends Layout {
@@ -138,8 +229,6 @@ class Text extends Layout {
 
     this.tokens = split(syl, renderContext, style)
 
-    this.textBoxes = textPolyCutup(polygons, lineHeight, width, height)
-
     this.dashWidth = renderContext.measureText('-').width
     this.spaceWidth = renderContext.measureText(' ').width
 
@@ -154,7 +243,10 @@ class Text extends Layout {
     return [updatedParentPosition]
   }
 
-  render(renderContext, {showBoxes = false, lineHeight = 20, style}) {
+  render(
+    renderContext,
+    {polygons, width, height, showBoxes = false, lineHeight = 20, style}
+  ) {
     const {dashWidth, spaceWidth} = this
 
     if (showBoxes) {
@@ -173,108 +265,32 @@ class Text extends Layout {
 
     Object.assign(renderContext, style) // we need to paint text with a particular style
 
-    let syllableCounter = 0
-    let tokenCursor = 0
-    let lastLineVisited = 0
-    for (const {startX, endX, startY, endY, lineIndex} of this.textBoxes) {
-      const finalX = this.box.x + startX
-      const finalY = this.box.y + startY
-      const finalW = endX - startX
-      const finalH = endY - startY
+    this.textBoxes = []
+    const steps = Math.floor(height / lineHeight)
+    for (let i = 0; i < steps - 1; i++) {
+      this.textBoxes.push(...getRowBoxes(i, lineHeight, width, polygons))
+    }
 
-      let tempWidth = 0
+    const tracking = {
+      syllableCounter: 0,
+      tokenCursor: 0,
+      lastLineVisited: 0
+    }
 
-      if (showBoxes) {
-        renderContext.strokeStyle = 'teal'
-        renderContext.strokeRect(finalX, finalY, finalW, finalH)
-      }
-
-      while (tempWidth < finalW) {
-        const token2 = this.tokens[tokenCursor]
-        if (!token2) {
-          break
-        }
-
-        if (token2.token === '\n') {
-          tokenCursor++ // don't render the newline token
-          lastLineVisited = lineIndex + 1
-          break // skip to the next box
-        }
-
-        if (lineIndex < lastLineVisited) {
-          break
-        }
-
-        if (token2.type === 'word') {
-          if (tempWidth > finalW - token2.width) {
-            lastLineVisited = lineIndex
-            break
-          }
-
-          renderContext.fillText(
-            token2.token,
-            finalX + tempWidth,
-            finalY + lineHeight
-          )
-
-          // if (showBoxes) {
-          //   renderContext.strokeStyle = 'rgba(127, 63, 195, 0.6)'
-          //   renderContext.strokeRect(finalX + tempWidth, finalY, token2.width, 20)
-          // }
-
-          tempWidth += token2.width + spaceWidth
-          tokenCursor++
-        } else if (token2.type === 'syllables') {
-          while (token2.token[syllableCounter]) {
-            const syl = token2.token[syllableCounter]
-            const meas = token2.measurements[syllableCounter].width
-
-            if (tempWidth + meas + dashWidth <= finalW) {
-              if (showBoxes) {
-                renderContext.strokeStyle = 'rgba(127, 63, 195, 0.6)'
-                renderContext.strokeRect(
-                  finalX + tempWidth,
-                  finalY,
-                  meas,
-                  lineHeight
-                )
-              }
-
-              renderContext.fillText(
-                syl,
-                finalX + tempWidth,
-                finalY + lineHeight
-              )
-              tempWidth += meas
-              syllableCounter++
-            } else if (syllableCounter > 0) {
-              renderContext.fillText(
-                '-',
-                finalX + tempWidth,
-                finalY + lineHeight
-              )
-              lastLineVisited = lineIndex
-              break
-            } else {
-              lastLineVisited = lineIndex
-              break
-            }
-          }
-
-          if (syllableCounter >= token2.token.length) {
-            syllableCounter = 0
-            tokenCursor++
-            tempWidth += spaceWidth
-          } else {
-            lastLineVisited = lineIndex
-            break
-          }
-        } else {
-          console.error('invalid type:', token2.type)
-        }
-      }
+    for (let x = 0; x < this.textBoxes.length; x++) {
+      processBox(
+        renderContext,
+        this.box,
+        this.textBoxes[x],
+        this.tokens,
+        tracking,
+        lineHeight,
+        spaceWidth,
+        dashWidth,
+        showBoxes
+      )
     }
   }
 }
 
-module.exports = {Text, textPolyCutup}
+module.exports = {Text}
