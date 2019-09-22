e1b30dd5518bfe77de20f9f94b2cba0f8bb76359
Max Harris <harris.max@gmail.com>
2019-09-22 15:24:30 -0700

    refactor spaced-line-vertical-center test

--- test/layout.js
+++ test/layout.js
@@ -25,6 +25,8 @@ const verticalLayoutRightAligned = require('./fixtures/vertical-layout-right-ali
 
 const spacedLineVerticalRight = require('./fixtures/spaced-line-vertical-right');
 
+const spacedLineVerticalCenter = require('./fixtures/spaced-line-vertical-center');
+
 tape('spaced-line-horizontal-left-with-margin', t => {
   const {canvas} = setupComponentTest(spacedLineHorizontalLeftWithMargin);
   screenshot('spaced-line-horizontal-left-with-margin', canvas, t);
@@ -56,56 +58,7 @@ tape('spaced-line vertical right', t => {
 });
 
 tape('spaced-line vertical center', t => {
-  const {c, renderRoot} = require('../src/layout');
-  const {Root, Margin, Label, SpacedLine} = proxyquire('../src/components', {
-    './log': t.fail
-  });
-
-  const canvas = createCanvas(WIDTH, HEIGHT);
-  const ctx = canvas.getContext('2d');
-  const marginA = 100;
-  const demo1 = ({x, y, width, height}) =>
-    c(
-      Root,
-      {x, y, width, height, color: 'black'},
-      c(
-        Margin,
-        {
-          top: marginA,
-          bottom: marginA,
-          left: marginA,
-          right: marginA,
-          showBoxes: true
-        },
-        c(
-          SpacedLine,
-          {mode: 'vertical', align: 'center'},
-          c(Label, {
-            font: 'sans',
-            color: 'white',
-            size: 25,
-            text: 'i',
-            showBoxes: true
-          }),
-          c(Label, {
-            font: 'sans',
-            color: 'white',
-            size: 25,
-            text: 'Wide',
-            showBoxes: true
-          }),
-          c(Label, {
-            font: 'sans',
-            color: 'white',
-            size: 25,
-            text: '|',
-            showBoxes: true
-          })
-        )
-      )
-    );
-
-  renderRoot(ctx, demo1({x: 0, y: 0, width: WIDTH, height: HEIGHT}));
+  const {canvas} = setupComponentTest(spacedLineVerticalCenter);
   screenshot('spaced-line-vertical-center', canvas, t);
 });
 
