c953a9bd062872a067bae65981007c79c2f2f9a7
Max Harris <harris.max@gmail.com>
2020-01-19 18:30:17 -0800

    update to latest node-canvas

--- test/picking.js
+++ test/picking.js
@@ -9,7 +9,7 @@ tape('click a label', t => {
   const onLabelClick = event => {
     t.equals(event.box.x, 0);
     t.equals(event.box.y, 0);
-    t.equals(event.box.width, 303.11964843749996);
+    t.equals(event.box.width, 303.47999999999996);
     t.equals(event.box.height, 59.4);
     t.equals(event.childBox, undefined);
     t.deepEquals(event.event, {
@@ -34,9 +34,9 @@ tape('click a label', t => {
 tape('click a button', t => {
   t.plan(8);
   const onButtonClick = event => {
-    t.equals(event.box.x, 104.65962890624998);
+    t.equals(event.box.x, 105.03999999999998);
     t.equals(event.box.y, 59.4);
-    t.equals(event.box.width, 93.800390625);
+    t.equals(event.box.width, 93.4);
     t.equals(event.box.height, 106);
     t.equals(event.childBox, undefined);
     t.equals(event.event, undefined);
@@ -56,9 +56,9 @@ tape('click a button', t => {
 tape('click a button inside a viewport', t => {
   t.plan(8);
   const onLabelClick = event => {
-    t.equals(event.box.x, 80.42003906249998);
+    t.equals(event.box.x, 80.37999999999998);
     t.equals(event.box.y, 145.10000000000002);
-    t.equals(event.box.width, 46.6702734375);
+    t.equals(event.box.width, 46.39);
     t.equals(event.box.height, 66.2);
     t.equals(event.childBox, undefined);
     t.equals(event.event, undefined);
